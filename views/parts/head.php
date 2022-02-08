b07a9eafe1473e9600d0cf0560c0028f13385d51
obeauch <obeauch@gmail.com>
2022-02-08 12:59:51 -0500

    nouvelle_police

--- views/parts/head.php
+++ views/parts/head.php
@@ -4,5 +4,10 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Anton&family=Noticia+Text&display=swap" rel="stylesheet">
 
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css2?family=Bakbak+One&display=swap" rel="stylesheet">
+
+
 <!-- Base est défini dans index.php -->
 <link rel="stylesheet" href="<?=BASE?>/public/css/style.css">
